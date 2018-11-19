790746ac6b99fa37e346e14cd7265b29c932af1a
russoturisto <russoturisto@gmail.com>
2018-11-18 21:03:53 -0700

    precise cube rotation, 15 degree increments

--- js/listener.js
+++ js/listener.js
@@ -61,7 +61,7 @@ function oMdTs(
 
     // ev.originalEvent.touches ? ev = ev.originalEvent.touches[0] : null
     let p = ev.touches ? ev.touches[0] : ev
-    console.log('---===<<<((( mouse start )))>>>===---')
+    // console.log('---===<<<((( mouse start )))>>>===---')
     mouse.start.x = p.screenX
     mouse.start.y = p.screenY
     dLM.ad('mousemove', oMmTm)('touchmove', oMmTm)
@@ -106,82 +106,82 @@ LM.ad(VP).ad('move-viewport', function (
     let lastCoords = mouseObject.last
 
     let dx, dy,
-        vx = 0,
-        vy = 0,
+        vx,
+        vy,
         moveX = 0,
         xBy = 0,
         moveY = 0,
-        yBy = 0,
-        directionChanged = 0
+        yBy = 0
+    // directionChanged = 0
 
     let now = new Date();
 
     if (!mouse.last) {
         mouse.last = lastCoords = mouse.start
         lastMove = now;
-        vx = directionVector(startCoords.x, newMouseLocation.x)
-        vy = directionVector(startCoords.y, newMouseLocation.y)
-    } else {
-        vx = directionVector(lastCoords.x, newMouseLocation.x)
-        let oldVx = directionVector(startCoords.x, lastCoords.x);
+        // vx = directionVector(startCoords.x, newMouseLocation.x)
+        // vy = directionVector(startCoords.y, newMouseLocation.y)
+        // } else {
+        // vx = directionVector(lastCoords.x, newMouseLocation.x)
+        // let oldVx = directionVector(startCoords.x, lastCoords.x);
         // console.log('old vx: ' + directionVector(startCoords.x, lastCoords.x))
         // console.log('new vx: ' + vx)
         // if the movement in x axis changed directionVector
-        if (oldVx[0] != vx[0] && oldVx[1] != 0) {
-            if (oldVx[1] > 2 || vx[1] > 2) {
-                console.log('X dir changed, old: ' + oldVx + ', new: ' + vx)
-                // set the last X coordinates as the start (of new directionVector in movement)
-                startCoords.x = lastCoords.x
-                directionChanged = 1
-            }
-            // else {
-            //     console.log('X directionVector change too small, old: ' + oldVx + ', new: ' + vx)
-            // }
-        }
-        vy = directionVector(lastCoords.y, newMouseLocation.y)
-        let oldVy = directionVector(startCoords.y, lastCoords.y)
+        // if (oldVx[0] != vx[0] && oldVx[1] != 0) {
+        //     if (oldVx[1] > 2 || vx[1] > 2) {
+        // console.log('X dir changed, old: ' + oldVx + ', new: ' + vx)
+        // set the last X coordinates as the start (of new directionVector in movement)
+        // startCoords.x = lastCoords.x
+        directionChanged = 1
+        // }
+        // else {
+        //     console.log('X directionVector change too small, old: ' + oldVx + ', new: ' + vx)
+        // }
+        // }
+        // vy = directionVector(lastCoords.y, newMouseLocation.y)
+        // let oldVy = directionVector(startCoords.y, lastCoords.y)
         // console.log('old vy: ' + directionVector(startCoords.y, lastCoords.y))
         // console.log('new vy: ' + vy)
         // if the movement in y axis changed directionVector
-        if (oldVy[0] != vy[0] && oldVy[1] != 0) {
-            if (oldVy[1] > 2 || vy[1] > 2) {
-                console.log('Y dir changed, old: ' + oldVy + ', new: ' + vy)
-                // set the last X coordinates as the start (of new directionVector in movement)
-                startCoords.y = lastCoords.y
-                directionChanged = 1
-            }
-            // else {
-            //     console.log('Y directionVector change too small, old: ' + oldVy + ', new: ' + vy)
-            // }
-        }
+        // if (oldVy[0] != vy[0] && oldVy[1] != 0) {
+        //     if (oldVy[1] > 2 || vy[1] > 2) {
+        // console.log('Y dir changed, old: ' + oldVy + ', new: ' + vy)
+        // set the last X coordinates as the start (of new directionVector in movement)
+        // startCoords.y = lastCoords.y
+        directionChanged = 1
+        // }
+        // else {
+        //     console.log('Y directionVector change too small, old: ' + oldVy + ', new: ' + vy)
+        // }
+        // }
     }
     // dx = movementInPixels(startCoords.x, newMouseLocation.x)
     // dy = movementInPixels(startCoords.y, newMouseLocation.y)
 
-    if (!directionChanged) {
-        vx = directionVector(startCoords.x, newMouseLocation.x)
-        vy = directionVector(startCoords.y, newMouseLocation.y)
-    }
+    // if (!directionChanged) {
+    vx = directionVector(startCoords.x, newMouseLocation.x)
+    vy = directionVector(startCoords.y, newMouseLocation.y)
+    // }
     // console.log('x: ' + vx)
-    console.log('y: ' + vy)
+    // console.log('y: ' + vy)
     dx = vx[1]
     dy = vy[1]
 
     // If general directionVector is in X
     if (dx >= 4 && dx / 4 > dy) {
-        console.log('dx >= 4 && dx / 4 > dy')
+        // console.log('dx >= 4 && dx / 4 > dy')
         yBy = vx[0]
         moveY = 1
     }
     // If general directionVector is in Y
     else if (dy >= 4 && dy / 4 > dx) {
-        console.log('dy >= 4 && dy / 4 > dx')
+        // console.log('dy >= 4 && dy / 4 > dx')
         xBy = -vy[0]
         moveX = 1
     }
     // Otherwise its in both x and y
     else if (dx >= 4 && dy >= 4) {
-        console.log('dx >= 4 && dy >= 4')
+        // console.log('dx >= 4 && dy >= 4')
         xBy = -vy[0]
         yBy = vx[0]
         moveX = 1
@@ -193,14 +193,16 @@ LM.ad(VP).ad('move-viewport', function (
         startCoords.y = lastCoords.y
         if (moveX || moveY) {
             // console.log('moving, time elapsed: ' + (now - lastMove))
-            if (directionChanged) {
-                console.log('moving, dir changed:  ' + directionChanged)
-            }
+            // if (directionChanged) {
+            //     // console.log('moving, dir changed:  ' + directionChanged)
+            //     alert('direction changed!');
+            // }
 
-            console.log("moveX: " + xBy + ", moveY: " + yBy)
+
+            // console.log("moveX: " + xBy + ", moveY: " + yBy)
             viewport.move(moveX, xBy, moveY, yBy)
-            lastMove = now
         }
+        lastMove = now
     }
     // else {
     //     console.log('staying, time elapsed: ' + (now - lastMove))
@@ -211,9 +213,9 @@ LM.ad(VP).ad('move-viewport', function (
     lastCoords.y = newMouseLocation.y
 })
 
-setTimeout(function () {
-    console.log('---===<<<((( TICK )))>>>===---');
-}, 4000)
+// setTimeout(function () {
+//     console.log('---===<<<((( TICK )))>>>===---');
+// }, 4000)
 
 function directionVector(fromPosition, toPosition) {
     let movementDirection, changeInPixels, range
@@ -239,7 +241,7 @@ function directionVector(fromPosition, toPosition) {
     return [movementDirection, changeInPixels, range]
 }
 
-function movementInPixels(fromPosition, toPosition) {
+/*function movementInPixels(fromPosition, toPosition) {
     // If both positions are non-negative
     if (toPosition >= 0 && fromPosition >= 0) {
         // If the position is growing
@@ -255,4 +257,4 @@ function movementInPixels(fromPosition, toPosition) {
             return fromPosition - toPosition
         }
     }
-}
+}*/
--- js/touch.js
+++ js/touch.js
@@ -1,6 +1,30 @@
-var Px = [0, 45, 90, 135, 180, 225, 270, 315]
-var Py = [0, 45, 90, 135, 180, 225, 270, 315]
+// var Px = [0, 5, 15, 30, 40, 45, 50, 60, 75, 85, 90, 95, 105, 120, 130, 135, 140, 150, 165, 175, 180, 185, 195, 210, 220, 225, 230, 240, 255, 265, 270, 275, 285, 300, 310, 315, 320, 330, 345, 355]
+// var Py = [0, 5, 15, 30, 40, 45, 50, 60, 75, 85, 90, 95, 105, 120, 130, 135, 140, 150, 165, 175, 180, 185, 195, 210, 220, 225, 230, 240, 255, 265, 270, 275, 285, 300, 310, 315, 320, 330, 345, 355]
 
+var Px = []
+var Py = []
+var MATRIX = []
+var DIVISIONS = 24
+
+for(var i = 0; i < DIVISIONS; i++) {
+    Px[i] = Py[i] = i * 15
+    var xMatrix = []
+    for(var j = 0; j < DIVISIONS; j++) {
+        // xMatrix.push([0, 0, 0, 0, 0, 0])
+    }
+    MATRIX.push(xMatrix)
+}
+// 3-D
+// 10, 10, 80       30, 10, 60      45, 45, 10
+// 5,  35, 60       15, 35, 50      33, 33, 33
+//                  20, 40, 30      30, 40, 30
+//                  10, 60, 30      20, 60, 20
+//                                  10, 90,  0      15, 85,  0
+
+// 2-D
+// 20, 80
+// 40, 60
+// 50, 50
 /*
 Need to be able to find 2 numbers to move by by:
     Starting rotation:
@@ -22,29 +46,53 @@ var M = [ // starting x rotations
 ]
  */
 
+function S(a, b, c, d, e, f) {
+    MATRIX[viewport.xi][viewport.yi] = [a, b, c, d, e, f]
+}
+
+function printMatrix() {
+    for(var i = 0; i < DIVISIONS; i++) {
+        var xMatrix = MATRIX[i]
+        for(var j = 0; j < DIVISIONS; j++) {
+            var yMatrix = xMatrix[j]
+            var str = ''
+            for(var k = 0; k < 6; k++) {
+                str = str + yMatrix[k] + '\t'
+            }
+            console.log(str)
+        }
+        console.log()
+        MATRIX.push(xMatrix)
+    }
+    console.log()
+    console.log()
+    console.log()
+}
 
 var mouse = {
         start: {}
     },
     touch = D.ontouchmove !== undefined,
     viewport = {
-        x: 0, // -35.2
-        xi: 0,
-        y: 0,
-        yi: 0, // 45
+        x: -30,
+        xi: -2,
+        y: -45,
+        yi: -3,
         el: gQ('#cube'),
         move(
             moveX, xBy, moveY, yBy
         ) {
             if (moveX)
-                this.x = moveCoordinates(Px, this.xi += xBy)
+                this.x = moveCoordinates(Px, this.xi += xBy)[0]
             if (moveY)
-                this.y = moveCoordinates(Py, this.yi += yBy)
+                this.y = moveCoordinates(Py, this.yi += yBy)[0]
 
             this.el.style["transform"] = "rotateX(" + this.x + "deg) rotateY(" + this.y + "deg)"
         },
         reset() {
-            this.move({x: 0, y: 0})
+            this.xi = 0
+            this.yi = 0
+            this.move(0, 0, 0, 0)
         }
     }
 
@@ -63,15 +111,19 @@ function moveCoordinates(
         multiplier = -1;
         currentIndex = -currentIndex;
     }
-    let page = Math.floor(currentIndex / 8)
-    let index = currentIndex % 8
+    let page = Math.floor(currentIndex / DIVISIONS)
+    let index = currentIndex % DIVISIONS
 
-    if (index === 8) {
+    if (index === DIVISIONS) {
         page++
         index = 0
     }
 
-    return (page * 360 + percentArray[index]) * multiplier;
+    let angle = percentArray[index];
+
+    let rotation = (page * 360 + angle) * multiplier;
+
+    return [rotation, angle, index, page, multiplier]
 }
 
 
