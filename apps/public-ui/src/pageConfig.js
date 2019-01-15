44dec26262bc991a2b0f46d32745b253380456d8
russoturisto <russoturisto@gmail.com>
2019-01-15 08:34:26 -0700

    refactoring & improving/adding new art

---
+++ apps/public-ui/src/pageConfig.js
@@ -0,0 +1,27 @@
+export var PAGE_CONF = {
+    cube: {
+        key: 'cube',
+        name: 'Trump\'s Policies',
+        rightMenu: true
+    },
+    chart: {
+        key: 'chart',
+        name: 'Trump\'s Policies',
+        rightMenu: true
+    },
+    createPoll1: {
+        key: 'createPoll1',
+        name: 'Trump\'s Policies',
+        rightMenu: false
+    },
+    selectPlace: {
+        key: 'selectPlace',
+        name: 'Trump\'s Policies',
+        rightMenu: false
+    },
+    selectDates: {
+        key: 'selectDates',
+        name: 'Trump\'s Policies',
+        rightMenu: false
+    },
+}
