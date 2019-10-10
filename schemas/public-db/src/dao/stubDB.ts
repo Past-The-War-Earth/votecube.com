5be0c3b7420e874cfe856aec7f90340303b08425
russoturisto <russoturisto@gmail.com>
2019-10-10 12:30:53 -0600

    finishing the screens, added some marketing material

--- schemas/public-db/src/dao/stubDB.ts
+++ schemas/public-db/src/dao/stubDB.ts
@@ -71,38 +71,52 @@ export const DB = {
 	}],
 	factors: [{
 		id: 1,
-		name: 'Security'
+		name: 'Future Burden'
+		// name: 'Security'
 	}, {
 		id: 2,
-		name: 'Economy'
+		name: 'Scientific Proof'
+		// name: 'Economy'
 	}, {
 		id: 3,
-		name: 'Equality',
+		name: 'Personal Impact',
+		// name: 'Equality',
 	}],
 	positions: [{
 		id: 1,
-		// name: ' . . . . . X+'
-		name: 'Trumps policies reduce military\'s over extension and let it focus on the real threats.'
+		// name: ' . . . . . X+',
+		name: 'We are shifting the burden to future generations'
+		// name: 'Trumps policies reduce military\'s over extension and let it focus on the real
+		// threats.'
 	}, {
 		id: 2,
-		// name: ' . . . . . X-'
-		name: 'Trump\'s policies reduce the trust in our alliances and hurt the stability of the world.'
+		// name: ' . . . . . X-',
+		name: ''
+		// name: 'Trump\'s policies reduce the trust in our alliances and hurt the stability of the
+		// world.'
 	}, {
 		id: 3,
 		// name: ' . . . . . Y+'
-		name: 'Trump\'s policies give profound confidence to companies and are good for the economy.'
+		name: 'There is enough evidence that climate change is man-made'
+		// name: 'Trump\'s policies give profound confidence to companies and are good for the
+		// economy.'
 	}, {
 		id: 4,
 		// name: ' . . . . . Y-'
-		name: 'Trump\'s policies have no real effect on the economy, it\'s purely cyclical.'
+		name: ''
+		// name: 'Trump\'s policies have no real effect on the economy, it\'s purely cyclical.'
 	}, {
 		id: 5,
 		// name: ' . . . . . Z+'
-		name: 'Trump\'s policies are better for equality because they are fighting the dominance of globalism and are bringing the jobs back.'
+		name: ''
+		// name: 'Trump\'s policies are better for equality because they are fighting the dominance
+		// of globalism and are bringing the jobs back.'
 	}, {
 		id: 6,
 		// name: ' . . . . . Z-'
-		name: 'Trump\'s policies are tailored with the interests of the wealthy in mind and hurt equality.'
+		name: 'I am personally not yet affected by climate change'
+		// name: 'Trump\'s policies are tailored with the interests of the wealthy in mind and hurt
+		// equality.'
 	}],
 	labels: [{
 		id: 1,
@@ -114,16 +128,26 @@ export const DB = {
 	polls: [{
 		endDate: new Date(new Date().getTime() + 3600000 * 75),
 		id: 1,
-		name: 'Trump\'s policies',
+		name: 'Act now to slow climate change?',
+		// name: 'Trump\'s policies',
+		outcomes: [{
+			id: 1,
+			key: 'A',
+			outcome: 'Trump\'s policies are generally beneficial.'
+		}, {
+			id: 2,
+			key: 'B',
+			outcome: 'Trump\'s policies are generally detrimental.'
+		}],
 		pollsContinents: [{
 			continent: {
-				'id': 'NorthAmerica'
+				id: 'NorthAmerica'
 			}
 		}],
 		pollsFactorsPositions: [{
 			axis: 'x',
 			color: {
-				id: 255 * 256 * 256
+				id: 255 * 256 * 256 + 255 * 256
 			},
 			factorPosition: {
 				id: 1,
@@ -133,7 +157,7 @@ export const DB = {
 		}, {
 			axis: 'x',
 			color: {
-				id: 255 * 256 * 256
+				id: 255 * 256 * 256 + 255 * 256
 			},
 			factorPosition: {
 				id: 2,
@@ -143,7 +167,7 @@ export const DB = {
 		}, {
 			axis: 'y',
 			color: {
-				id: 255 * 256 * 256 + 255 * 256
+				id: 184 * 256 * 256 + 55 * 256 + 200
 			},
 			factorPosition: {
 				id: 3,
@@ -153,7 +177,7 @@ export const DB = {
 		}, {
 			axis: 'y',
 			color: {
-				id: 255 * 256 * 256 + 255 * 256
+				id: 184 * 256 * 256 + 55 * 256 + 200
 			},
 			factorPosition: {
 				id: 4,
@@ -164,7 +188,7 @@ export const DB = {
 		}, {
 			axis: 'z',
 			color: {
-				id: 255 * 256
+				id: 253 * 256 * 256 + 120 * 256 + 15
 			},
 			factorPosition: {
 				id: 5,
@@ -174,7 +198,7 @@ export const DB = {
 		}, {
 			axis: 'z',
 			color: {
-				id: 255 * 256
+				id: 253 * 256 * 256 + 120 * 256 + 15
 			},
 			factorPosition: {
 				id: 6,
