77bb30faff47dbe0b4534dfd93554bfa3f74f95f
russoturisto <russoturisto@gmail.com>
2020-03-04 08:39:21 -0700

    Cleaned up rating schema.

--- schemas/relational-db/lib/ddl/poll/rating/PollRevisionRating.d.ts
+++ schemas/relational-db/lib/ddl/poll/rating/PollRevisionRating.d.ts
@@ -1,8 +1,7 @@
-import { UserPollRevisionRating_Id } from '../../../types/poll/user/UserPollRevisionRating';
+import { PollRevisionRating_Id, PollRevisionRating_Value } from '../../../types/poll/rating/PollRevisionRating';
 import { Rating } from '../../infrastructure/Rating/Rating';
 import { MutableActorRow } from '../../infrastructure/row/MutableActorRow';
 import { PollRevision } from '../revision/PollRevision';
-import { UserPollRevision } from './UserPollRevision';
 /**
  * User ratings for a particular Poll Revision (likely computed from
  * PollRevisionTranslationRating).
@@ -11,9 +10,9 @@ import { UserPollRevision } from './UserPollRevision';
  * the current Rating and is it a change in rating (of the same type) and
  * from what rating it was changed (so that it can decrement).
  */
-export declare class UserPollRevisionRating extends MutableActorRow {
-    id: UserPollRevisionRating_Id;
-    userPollRevision: UserPollRevision;
+export declare class PollRevisionRating extends MutableActorRow {
+    id: PollRevisionRating_Id;
+    value: PollRevisionRating_Value;
     pollRevision: PollRevision;
     rating: Rating;
 }
