f122e6539890021f43a802ac424c5d6213190f54
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2022-03-29 12:37:22 -0600

    Started on api and runtime application split.

---
+++ schemas/votecube/runtime/rollup.config.js
@@ -0,0 +1,37 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/runtime.ts',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: '../../../apps/public-ui/public/AIRport/apps/@votecube/votecube/build/bundle.js'
+	},
+	plugins: [
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true
+		}),
+		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
